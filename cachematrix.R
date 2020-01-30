d332d9c3a9fb368c6947d1b063a1755b3847a168
deepaks96 <60278877+deepaks96@users.noreply.github.com>
2020-01-30 15:56:02 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,39 @@
 ##Deepak Shekhar
 
-## makeCacheMatrix function helps us to first cache the given matrix and cacheSolve
-## helps to invert the cached matrix
+## makeCacheMatrix function helps us to first cache the given matrix and cacheSolve ##
 
-makeCacheMatrix <- function(x){
+## helps to invert the cached matrix ##
+
+makeCacheMatrix <- function(x = matrix){
   
-  n= sample(1:100,1,replace=T) 
-  x = matrix(1:n^2, nrow = n, ncol = n)      ## A 3x3 matrix generating values from 1 to 100
-                                              
+  inverse <- NULL
+  set <- fuction(mx) {
+     x <<- mx
+     inv <<- NULL
+  }
+  get <- function() x
   
+  setinverse <- function(inverse) 
+    inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse
   
 }
 
+## Now the cache matrix has been created. The second matrix will help in solving the inverse ##
 
+cacheSolve <- function(x, ...)   {       ## Cache Solve function retrieves inverse from cache ##
+   
+   inv <- x$getinverse()                 ## This gives us inverse of x ##
+   if(!is.null(inv)) {
+     
+     message ("We are getting cached data")
+     return(inv)
+     }
+   
+   data <- x$get()
+   inv <- solve(data, ...)               ## This gives us our final result ##
+   x$setinverse(inv)
+   inv
 
-                                         
-
-cacheSolve <- function(makeCacheMatrix) {
-  
-  solve(makeCacheMatrix)                     ## Return a matrix that is the inverse of 'x'
-
-  }
+}
